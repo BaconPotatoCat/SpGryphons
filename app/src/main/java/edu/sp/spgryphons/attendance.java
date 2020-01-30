a833b2b071a9022d589dccfd8d6881a67360f237
Thevagaran <56866836+Thevagaran@users.noreply.github.com>
2020-01-30 16:47:39 +0800

    theva

--- app/src/main/java/edu/sp/spgryphons/attendance.java
+++ app/src/main/java/edu/sp/spgryphons/attendance.java
@@ -3,6 +3,7 @@ package edu.sp.spgryphons;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -14,12 +15,15 @@ import java.util.ArrayList;
 
 public class attendance extends AppCompatActivity {
 
+    //welgjnajsbgjkbabg
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendance);
+
+        Log.d("name","Theva commmit");
 
 
     }
