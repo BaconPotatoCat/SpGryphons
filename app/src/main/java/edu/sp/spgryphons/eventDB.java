6ff24461b4760faf1cf8d66a27e34d15c1c09705
BaconPotatoCat <kobinfong11@gmail.com>
2020-01-31 18:47:47 +0800

    Added recycler view and able to read events from firebase

--- app/src/main/java/edu/sp/spgryphons/eventDB.java
+++ app/src/main/java/edu/sp/spgryphons/eventDB.java
@@ -1,27 +1,62 @@
 package edu.sp.spgryphons;
 
+import android.content.Context;
 import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class eventDB {
-
-    public static ArrayList<eventObj> getEventObj() {
-
-        ArrayList<eventObj> e = new ArrayList<eventObj>();
+    public static ArrayList<eventObj> getEventObj(Context context) {
+        final ArrayList<eventObj> e = new ArrayList<eventObj>();
         try {
-            //Enter code to get events from firebase database
-            // Do loop to retrieve all events and append to array
-            //while (rs.next()) {
-            //    eventObj ev = new eventObj(rs.getString("title"),rs.getString("date"),rs.getString("time"),rs.getString("desc"),rs.getFloat("longitude"),rs.getString("latitude"));
-            //    e.add(ev);
-            //}
-            eventObj ev = new eventObj("SPOH","7 Jan 2020","0900-1800", "Singapore Poly Open House event",1.3103459, 103.7742941);
-            e.add(ev);
-            ev = new eventObj("CTF","20 Jul 2020","0600-2359", "Capture the Flag event hosted by Singapore Poly",1.3103459, 103.7742941);
-            e.add(ev);
-            return e;
+            RequestQueue queue = Volley.newRequestQueue(context);
+            String url = "https://mapp-prac13.firebaseio.com/eventList.json";
+
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            Log.d("TAG", "Response is:" + response);
+                            try {
+                                JSONObject JObj = new JSONObject(response);
+                                int i=1;
+                                for(;;){
+                                    JSONObject even = JObj.getJSONObject("events");
+                                    try {
+                                        JSONObject eve = even.getJSONObject("event" + i);
+                                        Log.d("TAG","event"+i+" eve:"+eve);
+                                        eventObj ev = new eventObj(eve.getString("title"), eve.getString("date"),
+                                                eve.getString("time"), eve.getString("description"),
+                                                eve.getDouble("latitude"), eve.getDouble("longitude"));
+                                        e.add(ev);
+                                        i++;
+                                    } catch(JSONException exec) {
+                                        break;
+                                    }
+                                }
+                                events.mAdapter.notifyDataSetChanged();
+                            } catch (JSONException ex) {
+                                Log.d("TAG","JSON EXCEPTION:"+ex);
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.e("TAG","Failure to retrieve events from database.");
+                        }
+                    });
+            queue.add(stringRequest);
         } catch(Exception er) {
             Log.d("Error", "Failure to retrieve events from database");
         }
--- app/src/main/java/edu/sp/spgryphons/events.java
+++ app/src/main/java/edu/sp/spgryphons/events.java
@@ -7,25 +7,33 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 public class events extends AppCompatActivity {
     private RecyclerView mRecyclerView;
-    private eventsAdapter mAdapter;
+    public static eventsAdapter mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
 
         eventDB b = new eventDB();
-        ArrayList<eventObj> eventsArray = b.getEventObj();
+        ArrayList<eventObj> eventsArray = b.getEventObj(this);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.eventsView);
         mAdapter = new eventsAdapter(this, eventsArray);
         mRecyclerView.setAdapter(mAdapter);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
-
-
-
 }
