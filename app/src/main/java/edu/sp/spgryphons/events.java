5e2de182867165ba52ecd6646aac3d4cb516fe3e
BaconPotatoCat <kobinfong11@gmail.com>
2020-02-07 08:14:01 +0800

    Added regex for add events (complete)

--- app/src/main/java/edu/sp/spgryphons/events.java
+++ app/src/main/java/edu/sp/spgryphons/events.java
@@ -4,14 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 import android.content.SharedPreferences;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.Toast;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
-import java.util.List;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -19,32 +16,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 public class events extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     public static eventsAdapter mAdapter;
-    Button settingbtn;
-    Button Eventsbtn;
-    Button Projectsbtn;
-    Button Attendancebtn;
     Button Logout;
     Toolbar mToolbar;
-    FirebaseAuth mFirebaseAuth;
-    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
